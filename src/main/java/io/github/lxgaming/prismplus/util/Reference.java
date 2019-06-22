79fa32352eb706115e5b06ff0c373d5006a82093
Alex Thomson <LXGaming@users.noreply.github.com>
2019-06-22 18:49:48 +1200

    Fix URL

--- src/main/java/io/github/lxgaming/prismplus/util/Reference.java
+++ src/main/java/io/github/lxgaming/prismplus/util/Reference.java
@@ -24,5 +24,5 @@ public class Reference {
     public static final String DESCRIPTION = "Additional logging for Prism";
     public static final String AUTHORS = "LX_Gaming";
     public static final String SOURCE = "https://github.com/LXGaming/PrismPlus";
-    public static final String WEBSITE = "https://lxgaming.github.io";
+    public static final String WEBSITE = "https://lxgaming.github.io/";
 }
